65b55689883f7ab081191d4721bce8f2541ce570
unknown <patrickkeita80@yahoo.com>
2023-05-05 14:54:34 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,28 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(150);
+
+CREATE TABLE owners (
+    id       SERIAL PRIMARY KEY,
+    fullname VARCHAR(200) NOT NULL,
+    age      INT NOT NULL
+);
+
+CREATE TABLE specie (
+    id  SERIAL PRIMARY KEY,
+    name  VARCHAR(200) NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN id;
+
+ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+
+
+ALTER TABLE animals DROP COLUMN species;
+
+
+ALTER TABLE animals  ADD COLUMN species_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES specie(id);
+
+ALTER TABLE animals ADD COLUMN species_id INT,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
