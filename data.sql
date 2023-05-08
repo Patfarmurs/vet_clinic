a2ca001b6bbbfebf3c4138f02cd3c8c5587fac75
unknown <patrickkeita80@yahoo.com>
2023-05-08 12:56:58 -0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,7 +2,6 @@
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23), (2, 'Gabumon', '2018-11-15', 2, true, 8), (3, 'Pikachu', '2021-01-07', 1, false, 15.04), (4, 'Devimon', '2017-05-12', 5, true, 11);
 
-/* Project day 2 */
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES (5, 'Charmander', '2020-02-08', 0, false, 11), (6, 'Plantmon', '2021-11-15', 2, true, 5.7), (7, 'Squirtle', '1993-04-02', 3, false, 12.13), (8, 'Angemon', '2005-06-12', 1, true, 45), (9, 'Boarmon', '2005-06-07', 7, true, 20.04), (10, 'Blossom', '1998-10-13', 3, true, 17), (11, 'Ditto', '2022-05-14', 4, true, 22);
 
@@ -48,39 +47,33 @@ UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE fullname = 'Dean Winchester')
 WHERE name IN ('Angemon', 'Boarmon');
 
-INSERT INTO vets (name, age, date_of_graduation)
-VALUES
-('Williams Tatcher', 45, '2000-04-23'),
-('Maisy Smith', 26, '2019-01-17'),
-('Stephanie Mendez', 64, '1981-05-04'),
-('Jack Harkness', 38, '2008-06-08');
+INSERT INTO vets (name, age, date_of_graduation) 
+VALUES ('William Tatcher', 45, '2000-04-23'), 
+('Maisy SMith', 26, '2019-01-17'), 
+('Stephanie Mendez', 64, '1981-05-04'), 
+('Jack Harness', 38, '2008-06-08');
 
-INSERT INTO specializations (vets_id, species_id)
-VALUES
-(1, 1),
-(2, 2),
-(3, 1),
-(4, 2);
+INSERT INTO specializations (vet_id, species_id)
+ VALUES (1, 1),(3, 1),(3, 2),(4, 2);
 
-INSERT INTO visits (animal_id, vets_id, date_of_visits)
-VALUES
-(1, 1, '2020-05-24'),
-(1, 3, '2020-07-22'),
-(2, 4, '2021-02-02'),
-(5, 2, '2020-01-05'),
-(5, 2, '2020-03-08'),
-(5, 2, '2020-05-14'),
-(3, 3, '2021-05-04'),
-(9, 4, '2021-02-24'),
-(7, 2, '2019-12-21'),
-(7, 1, '2020-08-10'),
-(7, 2, '2021-04-07'),
-(10, 3, '2019-09-29'),
-(8, 4, '2020-10-03'),
-(8, 4, '2020-11-04'),
-(4, 2, '2019-01-24'),
-(4, 2, '2019-05-15'),
-(4, 2, '2020-02-27'),
-(4, 2, '2020-08-03'),
-(6, 3, '2020-05-24'),
-(6, 1, '2021-01-11');
+INSERT INTO visits (animal_id, vet_id, visited_date) 
+VALUES  (1, 1, '2020-05-24'), 
+(1, 3, '2020-07-22'), 
+(2, 4, '2021-02-02'), 
+(3, 2, '2020-01-05'),
+(3, 2, '2020-03-08'), 
+(3, 2, '2020-05-14'), 
+(4, 3, '2021-05-04'), 
+(5, 4, '2021-02-24'), 
+(6, 2, '2019-12-21'), 
+(6, 1, '2020-08-10'), 
+(6, 2, '2021-04-07'), 
+(7, 3, '2019-09-29'), 
+(8, 4, '2020-10-03'), 
+(8, 4, '2020-11-04'), 
+(9, 2, '2019-01-24'), 
+(9, 2, '2019-05-15'), 
+(9, 2, '2020-02-27'), 
+(9, 2, '2020-08-03'), 
+(10, 3, '2020-05-24'), 
+(10, 1, '2021-01-11');
